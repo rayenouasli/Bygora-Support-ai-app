feb7bfdae1ab69c7e0f4a8b662e41846488628db
Rayen Ouasli <rayenwasli82@gmail.com>
2025-02-11 14:17:56 +0100

    Initial Status

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -34,6 +34,16 @@ function SideBar() {
                 </div>
                 </Link>
             </li>
+            <li className="flex-1">
+                <Link href='/review-sessions'
+                className="hover:opacity-50 flex flex-col text-center lg:text-left lg:flex-row items-center gap-2 p-5 rounded-md bg-[#2991EE]">
+                      <SearchIcon className="h-6 w-6 lg:h-8 lg:w-8" />
+                <div className="hidden md:inline">
+                    <p className="text-xl">View</p>
+                    <p className="text-sm font-extralight">Sessions</p>
+                </div>
+                </Link>
+            </li>
         </ul>
       
     </div>
