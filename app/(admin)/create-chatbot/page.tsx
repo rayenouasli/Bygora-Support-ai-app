0d2f53ebc4e65e59b5f25609d2a5e60d43573e29
Rayen Ouasli <rayenwasli82@gmail.com>
2025-02-11 17:06:51 +0100

    initial status

--- app/(admin)/create-chatbot/page.tsx
+++ app/(admin)/create-chatbot/page.tsx
@@ -1,4 +1,6 @@
 import Avatar from "@/components/Avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function CreateChatbot() {
   return (
@@ -9,6 +11,15 @@ function CreateChatbot() {
             <h2 className="font-light">
                 Create a new chatbot to assist you in your conversations with your customers
             </h2>
+            <form className="flex flex-col md:flex-row gap-2 mt-5">
+                <Input
+                type="text"
+                placeholder="Chatbot Name..."
+                className="max-w-lg"
+                required
+                />
+                <Button>Create Chatbot</Button>
+            </form>
         </div>
         
     </div>
