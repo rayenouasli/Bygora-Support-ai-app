fdf6317242d4fda2952d604aa89ffde4b4e5439f
Rayen Ouasli <rayenwasli82@gmail.com>
2025-02-18 17:11:11 +0100

    Initial Commit

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,9 +1,29 @@
+import { serverClient } from "@/lib/server/serverClient";
 import { NextRequest } from "next/server";
+import { gql } from "@apollo/client";
 
 export async function POST(request:NextRequest){
     const {query , variables} = await request.json();
 
     try {
+       let result;
+
+        if (query.trim().startsWith("mutation")){
+            result = await serverClient.mutate({
+                mutation : gql`
+                ${query}
+                `,
+                variables,
+            });
+        }
+        else{
+            result = await serverClient.query({
+                query : gql`
+                ${query}
+                `,
+                variables,
+            }); 
+        }
         
     } catch (error) {
         
