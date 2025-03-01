df915d99e048a9ec2014c5b79066bb03295d193d
Rayen Ouasli <rayenwasli82@gmail.com>
2025-03-01 17:17:43 +0100

    Implementing toast notifications with shadcn sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ApolloProviderWrapper from "@/components/ApolloProvider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen flex">
         {children}
+
+        <Toaster position="bottom-center" />
       </body>
     </html>
     </ClerkProvider>
